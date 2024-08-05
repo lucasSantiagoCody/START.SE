3b4938ba4a617035f06e3d73b0144c033698f083
lucascody <lucassantiagocody@gmail.com>
2024-08-05 22:26:14 +0100

    Create CustomUser model

--- user/models.py
+++ user/models.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
-        
+
         if not email:
             raise ValueError('Email field must be set')
 
@@ -14,11 +14,28 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, email, password, **extra_fields):
+
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        
         if not extra_fields.get('is_staff'):
             raise ValueError('is_staff field must be set')
         if not extra_fields.get('is_superuser'):
             raise ValueError('is_superuser field must be set')
         
         self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=30, unique=False)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = ('email')
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
