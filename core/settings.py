33d98ee29481c60ac91ff68a971ad93dd9646f17
lucascody <lucassantiagocody@gmail.com>
2024-08-05 22:10:01 +0100

    Add MESSAGE_TAGS in settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,8 @@
+from django.contrib.messages import constants
 from pathlib import Path
 import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -115,3 +118,14 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Message tags
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
