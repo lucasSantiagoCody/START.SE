574b9954294db202e5ee6520513794a5dcb0d746
lucascody <lucassantiagocody@gmail.com>
2024-08-06 22:21:30 +0100

    create url pattern for login view

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup_url')
+    path('signup/', views.signup_view, name='signup_url'),
+    path('login/', views.login_view, name='login_url'),
 ]
