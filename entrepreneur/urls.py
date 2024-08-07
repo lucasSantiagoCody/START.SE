62724c14d0654f2bd417ecaeb9b99b440f184a9e
lucascody <lucassantiagocody@gmail.com>
2024-08-07 17:59:58 +0100

    Create url pattern for companies_view

--- entrepreneur/urls.py
+++ entrepreneur/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('register-company/', views.register_company_view, name='register_company_url'),
+    path('companies/', views.companies_view, name='companies_url'),
+
 ]
