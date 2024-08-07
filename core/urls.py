55f59df3c9e5e60d55d072fc87e2924b4bc1b130
lucascody <lucassantiagocody@gmail.com>
2024-08-07 18:13:28 +0100

    Update core's urls.py to setting MEDIA url pattenrn

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
+from django.conf.urls.static import static
 from django.urls import path, include
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
@@ -6,4 +8,4 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('entrepreneur/', include('entrepreneur.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
