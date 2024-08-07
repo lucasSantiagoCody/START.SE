5844d88a57754a0d30d4a4ab3a56a3db55896fa2
lucascody <lucassantiagocody@gmail.com>
2024-08-07 19:37:18 +0100

    Create url pattern for add_document_view

--- entrepreneur/urls.py
+++ entrepreneur/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('register-company/', views.register_company_view, name='register_company_url'),
     path('companies/', views.companies_view, name='companies_url'),
     path('company/<int:company_id>/', views.company_view, name='company_url'),
+    path('add_document/<int:company_id>/', views.add_document_view, name='add_document_url'),
+
 ]
