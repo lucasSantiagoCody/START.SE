62cd4fd7dc478e2755f28a32ebca764d4e4216a1
lucascody <lucassantiagocody@gmail.com>
2024-08-08 20:07:39 +0100

    Create url pattern to add_metric_view

--- entrepreneur/urls.py
+++ entrepreneur/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('company/<int:company_id>/', views.company_view, name='company_url'),
     path('add-document/<int:company_id>/', views.add_document_view, name='add_document_url'),
     path('delete-document/<int:document_id>/', views.delete_document_view, name='delete_document_url'),
-
+    path('add-metric/<int:company_id>/', views.add_metric_view, name='add_metric_url'),
 
 ]
