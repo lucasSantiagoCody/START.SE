2dc9245e9cef972980fc5d6b393652d2993e1004
lucascody <lucassantiagocody@gmail.com>
2024-08-08 17:53:25 +0100

    Update add_document url pattern to add-document

--- entrepreneur/urls.py
+++ entrepreneur/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register-company/', views.register_company_view, name='register_company_url'),
     path('companies/', views.companies_view, name='companies_url'),
     path('company/<int:company_id>/', views.company_view, name='company_url'),
-    path('add_document/<int:company_id>/', views.add_document_view, name='add_document_url'),
+    path('add-document/<int:company_id>/', views.add_document_view, name='add_document_url'),
     path('delete_document/<int:document_id>/', views.delete_document_view, name='delete_document_url'),
 
 
