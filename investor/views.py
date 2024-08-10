a93c6d47fc2e2ba16e5dc4d66d53b102c653f46a
lucascody <lucassantiagocody@gmail.com>
2024-08-10 22:34:34 +0100

    Create make_proposal_view

--- investor/views.py
+++ investor/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from entrepreneur.models import Company, Document
+from django.contrib.messages import constants
+from .models import InvestmentProposal
+from django.contrib import messages
+from django.urls import reverse
 
 
 
@@ -44,3 +48,41 @@ def company_details_view(request, company_id):
         context['documents' ] = documents
 
         return render(request, 'company_details.html', context)
+
+def make_proposal_view(request, company_id):
+    value = request.POST.get('value')
+    percentage = request.POST.get('percentage')
+    company = get_object_or_404(Company, id=company_id)
+
+    valuation = (100 * int(value)) / int(percentage)
+
+
+    accepted_proposals = InvestmentProposal.objects.filter(company=company).filter(status='PA')
+    total = 0
+    for accepted_proposal in accepted_proposals:
+        total = total + accepted_proposal.percentage
+
+    if total + int(percentage)  > company.percentage_equity:
+        messages.add_message(request, constants.WARNING, 'O percentual solicitado ultrapassa o percentual máximo.')
+    elif not value or not percentage:
+        messages.add_message(request, constants.WARNING, 'Prencha os campos valor para investor e percentual esperado')
+    elif valuation < (int(company.valuation) / 2):
+        messages.add_message(request, constants.WARNING, f'Seu valuation proposto foi R${valuation} e deve ser no mínimo R${company.valuation/2}')
+    else:
+
+        investment_proposal = InvestmentProposal(
+            value=value,
+            percentage=percentage,
+            company=company,
+            investor=request.user,
+        )
+
+        investment_proposal.save()
+
+        messages.add_message(request, constants.SUCCESS, f'Proposta enviada com sucesso')
+        return redirect(f'/investidores/assinar_contrato/{investment_proposal.id}')
+    
+    return redirect(reverse('company_details_url', kwargs={'company_id': company_id}))
+    
+
+    
