26d8230d49faa49d1e051f95921b9f03eecbc424
lucascody <lucassantiagocody@gmail.com>
2024-08-10 19:55:16 +0100

    Create InvestmentProposal model in investor app

--- investor/models.py
+++ investor/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from entrepreneur.models import Company
+from user.models import CustomUser
 
-# Create your models here.
+
+
+class InvestmentProposal(models.Model):
+
+    status_choices = (
+        ('AS', 'Aguardando assinatura'),
+        ('PE', 'Proposta enviada'),
+        ('PA', 'Proposta aceita'),
+        ('PR', 'Proposta recusada')
+    )
+
+    value = models.DecimalField(max_digits=9, decimal_places=2)
+    percentage = models.FloatField()
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
+    investor = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=2, choices=status_choices, default='AS')
+    selfie = models.FileField(upload_to="selfie", null=True, blank=True)
+    rg = models.FileField(upload_to="rg", null=True, blank=True)
+
+    def __str__(self):
+        return str(self.value)
