49cc3dd805500e572141758f1c9a230010e15a11
lucascody <lucassantiagocody@gmail.com>
2024-08-10 22:29:20 +0100

    Create url pattern for company_details_view

--- investor/urls.py
+++ investor/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('suggestion/', views.suggestion_view, name='suggestion_url')
+    path('suggestion/', views.suggestion_view, name='suggestion_url'),
+    path('company-details/<int:company_id>/', views.company_details_view, name='company_details_url'),
 ]
