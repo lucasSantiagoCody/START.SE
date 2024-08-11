5b0d51895e8bb8be23b7a50d9ff75d8af537ca98
lucascody <lucassantiagocody@gmail.com>
2024-08-11 13:32:05 +0100

    Rename company_details_url to entrepreneur_company_details_url

--- entrepreneur/urls.py
+++ entrepreneur/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('register-company/', views.register_company_view, name='register_company_url'),
     path('companies/', views.companies_view, name='companies_url'),
-    path('company-details/<int:company_id>/', views.company_details_view, name='company_details_url'),
+    path('company-details/<int:company_id>/', views.company_details_view, name='entrepreneur_company_details_url'),
     path('add-document/<int:company_id>/', views.add_document_view, name='add_document_url'),
     path('delete-document/<int:document_id>/', views.delete_document_view, name='delete_document_url'),
     path('add-metric/<int:company_id>/', views.add_metric_view, name='add_metric_url'),
